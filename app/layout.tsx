158068bf85919fbfcb2240b36d5c55f926c5bbdb
PedrooMota <pedrohmota2005@gmail.com>
2024-06-18 15:50:41 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Cfly",
+  // icons: {
+  //   icon: "favicon.ico",
+  // },
 };
 
 export default function RootLayout({
